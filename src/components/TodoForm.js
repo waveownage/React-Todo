5efaba3d1e9e51460de5789e7093bc74abba774b
Andre Freeman <asf@mail.usf.edu>
2020-04-12 19:27:53 -0400

    Added Line-Through Styling

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,34 +1,34 @@
 import React from "react";
 
 class TodoForm extends React.Component {
-    constructor() {
-        super();
-        this.state ={
-            item: ""
-        };
+  constructor() {
+    super();
+    this.state = {
+      item: ""
     };
-    handleChanges = e => {
-        this.setState({[e.target.name]: e.target.value})
-    };
-    submitItem = e => {
-        e.preventDefault();
+  }
+  handleChanges = e => {
+    this.setState({ item: e.target.value });
+  };
+  submitItem = e => {
+    e.preventDefault();
 
-        this.setState({ item: ""});
-        this.props.addItem(e, this.state.item);
-    };
-    render() {
-        return (
-            <form onSubmit={this.submitItem}>
-                <input
-                type="text"
-                name="item"
-                value={this.state.item}
-                onChange={this.handleChanges}
-                />
-                <button>Add</button>
-            </form>
-        )
-    }
+    this.setState({ item: "" });
+    this.props.addItem(e, this.state.item);
+  };
+  render() {
+    return (
+      <form onSubmit={this.submitItem}>
+        <input
+          type="text"
+          name="item"
+          value={this.state.item}
+          onChange={this.handleChanges}
+        />
+        <button>Add</button>
+      </form>
+    );
+  }
 }
 
 export default TodoForm;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,18 +1,18 @@
 import React from "react";
-import Item from "./Todo"
+import Item from "./Todo";
 
 const TodoList = props => {
-
-    return (
-        <div className="todo-list">
-            {props.items.map(item => (
-                <Item key={item.id} item={item} toggleItem={props.toggleItem} />
-            ))}
-            <button className="clear-btn" onClick={props.clearTodo}>
-                Clear Todo
-            </button>
-        </div>
-
-    )
-}
+  return (
+    <div>
+      <div className="todo-list">
+        {props.items.map(item => (
+          <Item key={item.id} item={item} toggleItem={props.toggleItem} />
+        ))}
+        <button className="clear-btn" onClick={props.clearCompleted}>
+          Clear Todo
+        </button>
+      </div>
+    </div>
+  );
+};
 export default TodoList;
